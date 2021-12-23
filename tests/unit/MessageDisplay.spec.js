80e0de8b85f1c6d304dc2afea4059af444df9bef
Rickson <rickson.simoes@hotmail.com>
2021-12-23 16:14:24 -0300

    :package: Axios & flushPromises/ :hammer: :wrench: Tests MessageDisplay

---
+++ tests/unit/MessageDisplay.spec.js
@@ -0,0 +1,38 @@
+import MessageDisplay from "@/components/MessageDisplay";
+import { mount } from "@vue/test-utils";
+import { getMessage } from "@/services/axios";
+import flushPromises from "flush-promises";
+
+jest.mock("@/services/axios");
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("MessageDisplay", () => {
+  it("Should call the api getMessage and show the data", async () => {
+    const mockMessage = "Hello from the DB";
+    getMessage.mockResolvedValueOnce({ text: mockMessage });
+
+    const wrapper = mount(MessageDisplay);
+
+    await flushPromises();
+    expect(getMessage).toHaveBeenCalledTimes(1);
+
+    const message = wrapper.find("[data-testid='message']").text();
+    expect(message).toEqual(mockMessage);
+  });
+
+  it("Should display an error when getMessage call fails", async () => {
+    const mockError = "Oops! Something went wrong.";
+    getMessage.mockRejectedValueOnce(mockError);
+
+    const wrapper = mount(MessageDisplay);
+
+    await flushPromises();
+    expect(getMessage).toHaveBeenCalledTimes(1);
+
+    const message = wrapper.find("[data-testid='message-error']").text();
+    expect(message).toEqual(mockError);
+  });
+});
